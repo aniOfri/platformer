3714733f329a58f30886d4198f9dcd106a17dd83
aniOfri <46932810+aniOfri@users.noreply.github.com>
2021-03-17 23:14:43 +0200

    Added server and const file

---
+++ const.py
@@ -0,0 +1,5 @@
+import pygame
+
+width = 500
+height = 700
+win = pygame.display.set_mode((width, height))
---
+++ server.py
@@ -0,0 +1,74 @@
+import socket
+from const import *
+from platformer import Platformer
+from _thread import *
+import pickle
+
+
+server = "0.0.0.0"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(2)
+print("Waiting for a connection, Server started..")
+
+connected = set()
+games = {}
+count = 0
+
+
+def threaded_client(conn, p, game_id):
+    global count
+    conn.send(str.encode(str(p)))
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
+
+            if game_id in games:
+                pfmr = games[game_id]
+
+                if not data:
+                    break
+                # else:
+                #    if data == "reset":
+
+                reply = pfmr
+                conn.sendall(pickle.dumps(reply))
+            else:
+                break
+        except:
+            break
+
+    print("Lost connection..")
+    try:
+        del games[game_id]
+        print("Closing game.. ", game_id)
+    except:
+        pass
+    count -= 1
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    count += 1
+    p = 0
+    game_id = (count - 1) // 2
+    if count % 2 == 1:
+        games[game_id] = Platformer(game_id, width/2, height-100)
+        print("Creating a new game...")
+    else:
+        games[game_id].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, game_id))
